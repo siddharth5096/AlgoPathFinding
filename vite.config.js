c24cba8d792034fc53c62ef50d846d704647e9b1
siddharth rout <srout5096@gmail.com>
2025-01-03 18:51:05 +0530

    Deplyment Fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [react()],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://github.com/siddharth5096/AlgoPathFinding", // Replace <REPO_NAME> with the name of your repository
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//     plugins: [react()],
+//     base: "https://github.com/siddharth5096/AlgoPathFinding", // Replace <REPO_NAME> with the name of your repository
+// });
 
