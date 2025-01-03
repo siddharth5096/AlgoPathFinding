43f663cd4f7a6c9853df38b68b2d0b73e8c305d9
siddharth rout <srout5096@gmail.com>
2025-01-03 12:35:04 +0530

    First Commit

---
+++ src/config.js
@@ -0,0 +1,39 @@
+export const MAP_STYLE = "./map_style.json";
+
+export const INITIAL_VIEW_STATE = {
+    longitude: 77.2193,
+    latitude: 28.6315,
+    zoom: 13,
+    pitch: 0,
+    bearing: 0
+};
+
+export const INITIAL_COLORS = {
+    startNodeFill: [70, 183, 128],
+    startNodeBorder: [255, 255, 255],
+    endNodeFill: [152, 4, 12],
+    endNodeBorder: [0, 0, 0],
+    path: [70, 183, 128],
+    route: [165, 13, 32],
+};
+
+export const LOCATIONS = [
+    { name: "Mumbai", latitude: 19.076, longitude: 72.877 },
+    { name: "Delhi", latitude: 28.613, longitude: 77.209 },
+    { name: "Bangalore", latitude: 12.971, longitude: 77.594 },
+    { name: "Hyderabad", latitude: 17.385, longitude: 78.486 },
+    { name: "Chennai", latitude: 13.082, longitude: 80.270 },
+    { name: "Kolkata", latitude: 22.572, longitude: 88.363 },
+    { name: "Pune", latitude: 18.520, longitude: 73.857 },
+    { name: "Ahmedabad", latitude: 23.022, longitude: 72.571 },
+    { name: "Jaipur", latitude: 26.912, longitude: 75.787 },
+    { name: "Lucknow", latitude: 26.846, longitude: 80.946 },
+    { name: "Bhopal", latitude: 23.259, longitude: 77.412 },
+    { name: "Kochi", latitude: 9.931, longitude: 76.267 },
+    { name: "Chandigarh", latitude: 30.733, longitude: 76.779 },
+    { name: "Patna", latitude: 25.594, longitude: 85.137 },
+    { name: "Visakhapatnam", latitude: 17.686, longitude: 83.218 },
+    { name: "Surat", latitude: 21.170, longitude: 72.831 },
+    { name: "Goa", latitude: 15.299, longitude: 74.124 },
+    { name: "Bhubaneswar", latitude: 20.296, longitude: 85.824 }
+];
